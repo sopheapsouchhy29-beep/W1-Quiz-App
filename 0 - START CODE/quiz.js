72814f6cea3db6559d568d465a7fbd9fedcd88d9
Sopheap Souchhy <224700986+sopheapsouchhy29-beep@users.noreply.github.com>
2026-03-22 21:27:13 +0700

    first commit

---
+++ 0 - START CODE/quiz.js
@@ -0,0 +1,75 @@
+// DOMS ELEMENTS  ---------------------------------------------------------
+const dom_quiz = document.querySelector("#quiz");
+const dom_question = document.querySelector("#question");
+const dom_choiceA = document.querySelector("#A");
+const dom_choiceB = document.querySelector("#B");
+const dom_choiceC = document.querySelector("#C");
+const dom_choiceD = document.querySelector("#D");
+const dom_score = document.querySelector("#score");
+const dom_start = document.querySelector("#start");
+
+dom_start.addEventListener("click", onStart);
+
+// DATA  ---------------------------------------------------------
+let questions = [
+  {
+    title: "What does HTML stand for?",
+    choiceA: "Hi Thierry More Laught",
+    choiceB: "How To move Left",
+    choiceC: "Ho Theary Missed the Laundry !",
+    choiceD: "Hypertext Markup Language",
+    correct: "D",
+  },
+  {
+    title: "What does CSS stand for?",
+    choiceA: "Cisco and Super Start",
+    choiceB: "Ci So Sa",
+    choiceC: "Cascading Style Sheets ",
+    choiceD: "I don't know !",
+    correct: "C",
+  },
+  {
+    title: "What does JS stand for?",
+    choiceA: "Junior stars",
+    choiceB: "Justing Star",
+    choiceC: "Javascript",
+    choiceD: "RonanScript",
+    correct: "C",
+  },
+];
+let runningQuestionIndex = 0;
+let score = 0;
+
+// FUNCTIONS ---------------------------------------------------------
+
+// Hide a given element
+function hide(element) {
+  // TODO
+}
+
+function show(element) {
+  // TODO
+}
+
+function onStart() {
+  // Render the current question
+  // Display the quiz view,
+}
+
+function renderQuestion() {
+  // Render the current question on the quiz view
+}
+
+function onPlayerSubmit(answer) {
+  // Update the score, display the next question or the score view
+}
+
+function renderSCore() {
+  // calculate the amount of question percent answered by the user
+  // choose the image based on the scorePerCent
+}
+
+// FUNCTIONS ---------------------------------------------------------
+show(dom_start);
+hide(dom_quiz);
+hide(dom_score);
